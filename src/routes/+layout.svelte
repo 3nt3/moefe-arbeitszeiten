156e295cca993bd9e114f8b79a7f663289ffc486
3nt3 <gott@3nt3.de>
2023-11-04 21:19:04 +0100

    add light/dark mode
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,15 @@
+<script>
+  import { autoModeWatcher } from "@skeletonlabs/skeleton";
+</script>
+
+<svelte:head
+  >{@html `<script>${autoModeWatcher.toString()} autoModeWatcher();</script>`}</svelte:head
+>
+
 <slot />
 
 <style>
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { LightSwitch } from "@skeletonlabs/skeleton";
+
   const months = [
     "Januar",
     "Februar",
@@ -25,45 +27,63 @@
   }
 </script>
 
-<main
-  class="bg-secondary-300 w-screen h-screen flex justify-center items-center p-8"
->
-  <div
-    class="bg-zinc-50 p-12 md:p-24 rounded-2xl shadow-2xl gap-6 flex flex-col max-w-[550px] w-full"
-  >
-    <h1 class="h2">
-      <span
-        class="bg-gradient-to-br font-bold from-blue-600 to-cyan-400 bg-clip-text text-transparent box-decoration-clone"
-        >Arbeitszeitdings</span
-      >
-    </h1>
-    <blockquote class="blockquote">
-      Arbeiten? Das steht auf meiner Not-To-Do-Liste.
+<main class="w-screen min-h-screen flex-col flex">
+  <nav class="w-screen flex justify-center items-center py-8">
+    <div class="flex flex-col items-center text-surface-700 w-full">
+      <h1 class="text-3xl tracking-widest text-surface-700 -mb-1">
+        ARBEITSZEITDINGS
+      </h1>
+      <span class="uppercase">est. 2023</span>
+    </div>
+  </nav>
+  <div class="w-full h-full flex justify-center items-center p-8">
+    <div
+      class="card p-12 md:p-24 rounded-2xl shadow-2xl gap-6 flex flex-col max-w-[550px] w-full mt-8"
+    >
+      <h1 class="h2 flex gap-2 justify-between items-center">
+        <span class="font-bold">Arbeitszeitdings</span>
+        <LightSwitch class="mb-1 mr-1" />
+      </h1>
+      <blockquote class="blockquote">
+        Arbeiten? Das steht auf meiner Not-To-Do-Liste.
 
-      <p class="mt-2">
-        &mdash; <cite>Das Känguru</cite>
-      </p>
-    </blockquote>
-    <form method="GET" action="/generate" class="flex flex-col">
-      <label for="month" class="label">Monat *</label>
-      <select
-        name="month"
-        class="select mb-2"
-        on:change={monthChanged}
-        bind:value={monthIndex}
-        class:input-error={monthIndex === undefined}
-      >
-        {#each months as month}
-          <option value={month.index}>{month.month}</option>
-        {/each}
-      </select>
-      <label class="label" for="year">Jahr *</label>
-      <input name="year" type="number" class="input" bind:value={year} />
-      <input type="submit" value="Generieren" class="mt-6 btn variant-filled" />
-    </form>
-  </div>
+        <p class="mt-2">
+          &mdash; <cite>Das Känguru</cite>
+        </p>
+      </blockquote>
+      <form method="GET" action="/generate" class="flex flex-col">
+        <label for="month" class="label">Monat *</label>
+        <select
+          required
+          name="month"
+          class="select mb-2"
+          on:change={monthChanged}
+          bind:value={monthIndex}
+          class:input-error={monthIndex === undefined}
+        >
+          {#each months as month}
+            <option value={month.index}>{month.month}</option>
+          {/each}
+        </select>
+        <label class="label" for="year">Jahr *</label>
+        <input
+          required
+          name="year"
+          type="number"
+          class="input"
+          bind:value={year}
+          class:input-error={!year}
+        />
+        <input
+          type="submit"
+          value="Generieren"
+          class="mt-6 btn variant-filled"
+        />
+      </form>
+    </div>
 
-  <div class="absolute bottom-0 p-4">
-    Made with ❤️ by <a href="https://3nt3.de">Nia</a> for 🐦
+    <div class="absolute bottom-0 p-4">
+      Made with ❤️ by <a href="https://3nt3.de">Nia</a> for 🐦
+    </div>
   </div>
 </main>
